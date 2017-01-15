eef3c46e8ab104f842c3c95e4f33120e67d589b2
Eddie Kao <eddie@digik.com.tw>
2017-01-15 11:39:26 +0800

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def hi
+    "hello"
+  end
+
+  def show_gender(candidate)
+    if candidate.gender == "1"
+      "男"
+    elsif candidate.gender == "2"
+      "女"
+    else
+      "不知道"
+    end
+  end
+
+  def candidate_name(candidate)
+    if candidate.gender == "1"
+      "#{candidate.name} 先生"
+    elsif candidate.gender == "2"
+      "#{candidate.name} 女士"
+    else
+      candidate.name
+    end
+  end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,13 @@
 class Candidate < ApplicationRecord
   validates :name, :party, presence: true
+
+  def display_name
+    if gender == "1"
+      "#{name} 先生"
+    elsif gender == "2"
+      "#{name} 女士"
+    else
+      name
+    end
+  end
 end
